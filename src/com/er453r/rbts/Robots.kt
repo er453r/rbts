d3f6b0b568347cd57b6e89e97398c25b668c92a1
Marcin Kotz <marcin.kotz@ives-system.com>
2019-09-23 15:06:44 +0200

    initial connection attempts

--- src/com/er453r/rbts/Robots.kt
+++ src/com/er453r/rbts/Robots.kt
@@ -1,5 +1,6 @@
 package com.er453r.rbts
 
+import io.grpc.ManagedChannelBuilder
 import mu.KotlinLogging
 
 class Robots(name: String) {
@@ -7,5 +8,13 @@ class Robots(name: String) {
 
     init {
         log.info { "Starting $name" }
+
+        val channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+            .usePlaintext()
+            .build()
+
+
+
+        channel.shutdown()
     }
 }
